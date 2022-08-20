adef9d70629691d0e309a69ef72ef7c7f0515a08
Yutsuki Zaitsu <89077321+YuiZaitsu@users.noreply.github.com>
2022-08-20 17:10:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function preload(){
+
+}
+
+function setup() {
+canvas= createCanvas(600,500);
+canvas.position(100,250);
+video=createCapture(VIDEO);
+video.hide();
+tint_color="";
+}
+
+function draw() {
+    image(video,100,80,300,250   );
+fill("red");
+stroke("blue");
+
+    circle(40,40,50);
+    circle(460,40,50);
+    circle(40,360,50);
+    circle(460,360,50);
+    fill("yellow");
+    stroke("green");
+    
+    rect(70, 27, 375, 25);
+    rect(70, 350, 375, 25);
+    rect(30, 63, 25, 275);
+    rect(448, 63, 25, 275);
+
+     
+}
+
+
+function take_snapshot() {
+    save("mypicture.png");
+    }
+
